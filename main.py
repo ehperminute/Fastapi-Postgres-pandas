5697d0c0b71c2c8b46eeb87319eaac714101e706
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-06-28 20:47:11 +0000

    v

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ def get_products_revenue():
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("""
-                    SELECT p.id, p.name, SUM(p.price * s.quantity)                      
+                    SELECT p.id, p.name, SUM(p.price * s.quantity)  AS revenue                    
                     FROM products p 
                         JOIN sales s ON s.product_id = p.id
-                    GROUP BY p.id
-                    ORDER BY 3 DESC;
+                    GROUP BY p.id, p.name
+                    ORDER BY revenue DESC;
                     """)
     rows = cursor.fetchall()
 
@@ -35,4 +35,25 @@ def get_products_revenue():
     conn.close()
     return {"revenue_products": [{"id": product_id, "name": product_name, "revenue": revenue} 
                                     for product_id, product_name, revenue in rows]}
+
+@app.get("/stats/customers_revenue")
+def customers_revenue():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+                    SELECT c.id, c.name, SUM(p.price * s.quantity)  AS revenue                    
+                    FROM products p 
+                        JOIN sales s ON s.product_id = p.id
+                        JOIN customers c ON s.customer_id = c.id
+                    GROUP BY c.id, c.name
+                    ORDER BY revenue DESC;
+                    """)
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return {"revenue_customers": [{"id": product_id, "name": product_name, "revenue": revenue} 
+                                    for product_id, product_name, revenue in rows]}
+
+
 
