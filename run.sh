767e4575c17eadbaba1311a6cab4289909fe0eb6
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-06-27 20:04:38 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 docker compose up -d
-until pg_isready -h localhost -p 5432 -U user
-do
-    echo "Waiting for PostgreSQL..."
-    sleep 1
-done
+
 python init_db.py
 
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
