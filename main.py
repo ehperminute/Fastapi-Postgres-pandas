73131cdc9a00d8e9e7698f303ff744600281e777
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-06-28 13:30:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,9 @@ from database import get_connection
 
 app = FastAPI()
 
-@app.get("/customers")
-def get_customers():
+@app.get("/stats/revenue")
+def get_revenue():
     conn = get_connection()
     cursor = conn.cursor()
+    cursor.execute()
 
-    cursor.execute("SELECT id, name, country FROM customers ORDER BY id")
-    rows = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    customers = [
-        {"id": row[0], "name": row[1], "country": row[2]}
-        for row in rows
-    ]
-
-    return {"customers": customers}
